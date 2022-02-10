c8d8683b92a40251ec62367734bc8c27ecae4174
eleanorxie <99051921+eleanorxie@users.noreply.github.com>
2022-02-10 14:58:51 -0800

    Inheritance Assignment 1

--- Inheritance Assignment 1/src/Main.java
+++ Inheritance Assignment 1/src/Main.java
@@ -4,5 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Planet planet = new GasPlanet(1, "1", true, Color.red);
         System.out.println(planet);
-    }
+      }
 }
